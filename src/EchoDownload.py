3f9e199fbf9569f45d6cfbb0939d85df9b0df606
hokket <mateus_fer@hotmail.com>
2025-05-02 15:10:33 -0300

    Adicao de loop para execuxao

--- src/EchoDownload.py
+++ src/EchoDownload.py
@@ -1,65 +1,90 @@
 #!/usr/bin/env python3
 
+import sys
 import yt_dlp as youtube_dl
 import os
-import time
-from tqdm import tqdm  # Biblioteca para animação de carregamento
+
 
 # Título do programa
-print("\033[94m╔════════════════════════════════════════╗")
-print("║        Bem-vindo ao EchoDownload!      ║")
-print("╚════════════════════════════════════════╝\033[0m")
+print("\033[94m╔═══════════════════════════════════════════════╗")
+print("║           Bem-vindo ao EchoDownload!          ║")
+print("║              Download de Vídeos               ║")
+print("║                                               ║")
+print("║              Versão 1.0 - 2025                ║")
+print("║               Desenvolvido em                 ║")
+print("║  https://github.com/HOkket/EchoDownload.git   ║")
+print("╚═══════════════════════════════════════════════╝\033[0m")
 
+rodar = "SIM"
 
-url = input("Digite o URL do vídeo do YouTube: ")  # URL do vídeo a ser baixado
-print("\033[93mAguarde enquanto o programa verifica o URL...\033[0m")
-# Verifica se o URL é válido
-while not url.startswith("https://youtu.be") and not url.startswith("https://youtube.com"):
-    print("\033[91mURL inválido. Por favor, insira um URL do YouTube válido.\033[0m")
+# inicio do loop
+while rodar == "SIM" or rodar == "S" or rodar == "sim" or rodar == "s":
+    # URL do vídeo a ser baixado
     url = input("Digite o URL do vídeo do YouTube: ")
+    print("\033[93mAguarde enquanto o programa verifica o URL...\033[0m")
+#  Verifica se o URL é válido
+    while not url.startswith("https://youtu.be") and not url.startswith("https://youtube.com"):
+        print(
+            "\033[91mURL inválido. Por favor, insira um URL do YouTube válido.\033[0m")
+        url = input("Digite o URL do vídeo do YouTube: ")
 
 
-# Voltar para 'usuário_padrão' se USER não estiver definido
-usuário = os.getenv('USER', 'usuário_padrão')
-output_local = f"/home/{usuário}/Downloads"  # Local onde o vídeo será salvo
+# Voltar para 'usuário_padrão' se USER não estiver definidoimport time
+
+
+def animated_download():
+    for i in range(101):
+        time.sleep(0.05)  # Simula o tempo de download
+        sys.stdout.write(
+            f"\rDownloading... [{'#' * (i // 2)}{' ' * (50 - i // 2)}] {i}%")
+        sys.stdout.flush()
+    usuário = os.getenv('USER', 'usuário_padrão')
+    # Local onde o vídeo será salvo
+    output_local = f"/home/{usuário}/Downloads"
 
 # Verifica se o diretório de saída existe, se não, cria
-if not os.path.exists(output_local):
-    os.makedirs(output_local)
+    if not os.path.exists(output_local):
+        os.makedirs(output_local)
 
 # Configurações para o yt-dlp
-ydl_opts = {
-    'format': 'bestvideo+bestaudio/best',
-    'merge_output_format': 'mp4',
-    'outtmpl': output_local + '/%(title)s.%(ext)s',
-    'noplaylist': True,
-    'quiet': True,
-}
-print("\033[91mIniciando o download...\033[0m")
-print("\033[91mPor favor, aguarde enquanto o download está em andamento...\033[0m")
-
-# Animação de carregamento
-
-
-def loading_animation():
-    for _ in tqdm(range(100), desc="\033[92mBaixando\033[0m", ascii=True):
-        time.sleep(0.05)
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',
+        'merge_output_format': 'mp4',
+        'outtmpl': output_local + '/%(title)s.%(ext)s',
+        'noplaylist': True,
+        'quiet': True,
+    }
+    print("\033[91mIniciando o download...\033[0m")
+    print(
+        "\033[91mPor favor, aguarde enquanto o download está em andamento...\033[0m")
 
 
 # Usa o yt-dlp para baixar o vídeo e áudio no melhor formato disponível.
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    try:
-        loading_animation()  # Exibe a animação de carregamento
-        ydl.download([url])
-        print(
-            f"\033[92mDownload concluído com sucesso! O arquivo foi salvo em: {output_local}\033[0m")
-    except Exception as e:
-        print(f"Ocorreu um erro: {e}")
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            # Exibe a animação de carregamento
+            ydl.download([url])
+            print(
+                f"\033[92mDownload concluído com sucesso! O arquivo foi salvo em: {output_local}\033[0m")
+        except Exception as e:
+            print(f"Ocorreu um erro: {e}")
 
-# Fim do programa
-print("\033[94m╔════════════════════════════════════════╗")
-print("║       Obrigado por usar o EchoDownload!║")
-print("╚════════════════════════════════════════╝\033[0m")
-print("\033[0m")
-
-exit(0)
+    # Pergunta ao usuário se deseja continuar
+    rodar = input("Deseja continuar? (SIM/NAO): ").strip().upper()
+    if rodar == "NAO" or rodar == "N" or rodar == "nao" or rodar == "n":
+        print("\033[91mSaindo do programa...\033[0m")
+        # Fim do programa
+        print("\033[94m╔═══════════════════════════════════════════════╗")
+        print("║       Obrigado por usar o EchoDownload!       ║")
+        print("╚═══════════════════════════════════════════════╝\033[0m")
+        print("\033[0m")
+        print("Se esse programa te ajudou, considere avaliar o projeto em:")
+        print("       https://github.com/HOkket/EchoDownload.git ")
+        exit()
+    elif rodar != "SIM" and rodar != "S" and rodar != "sim" and rodar != "s":
+        while rodar != "SIM" and rodar != "NAO" and rodar != "S" and rodar != "N" and rodar != "sim" and rodar != "s" and rodar != "nao" and rodar != "n":
+            print(
+                "\033[91mOpção inválida. Por favor, insira 'SIM' ou 'NAO'.\033[0m")
+            rodar = input("Deseja continuar? (SIM/NAO): ").strip().upper()
+        # Fim do loop
+# Fim do loop
